8f40bc45d7d17d951c71d5927e8a0ba8738bbb0b
FGallego7 <franigallegoox@gmail.com>
2024-04-30 15:32:19 -0300

    Finalizado componente de resume

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -7,7 +7,20 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent {
-  constructor(private titleService: Title){
+  isWorkExperienceOpen: boolean = false;
+  isEducOpen: boolean = false;
+  isSkillsOpen: boolean = false;
+
+  constructor(private titleService: Title, private renderer: Renderer2){
     this.titleService.setTitle('Francisco Gallego - Resume')
+  }
+
+  DownloadCv(){
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../../assets/CVFranciscoGallego2A.pdf');
+    link.setAttribute('download', 'CVFranciscoGallego2A.pdf');
+    link.click();
+    link.remove();
   }
 }
