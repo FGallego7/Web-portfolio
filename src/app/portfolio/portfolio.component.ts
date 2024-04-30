d0783b2d2c2ae9914dbc10a60d6f7951355ef8b8
FGallego7 <franigallegoox@gmail.com>
2024-04-30 15:30:15 -0300

    Modificado boton de filtrar

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -2,6 +2,7 @@ import { ProjectsService } from './../_services/projects.service';
 import { Project } from './../_models/project';
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Tag } from '../_models/tag';
 
 @Component({
   selector: 'app-portfolio',
@@ -12,11 +13,60 @@ export class PortfolioComponent implements OnInit  {
 
   projects = {} as Project[];
 
+  isCollapsed: boolean = false;
+  python: boolean = false;
+  angular: boolean = false;
+  java: boolean = false;
+  cSharp: boolean = false;
+  typeScript: boolean = false;
+  isFiltering: boolean = false;
+
+
   constructor(private titleService: Title, private projectService: ProjectsService){
     this.titleService.setTitle('Francisco Gallego - Portfolio')
   }
 
   ngOnInit(): void {
+    this.projects = this.projectService.GetAllProjects();
+  }
+
+  Filter(){
+    let filter: Tag[] = [];
+
+    if(this.python){
+      filter.push(Tag.PYTHON)
+    }
+    if(this.angular){
+      filter.push(Tag.ANGULAR)
+    }
+    if(this.java){
+      filter.push(Tag.JAVA)
+    }
+    if(this.cSharp){
+      filter.push(Tag.C_SHARP)
+    }
+    if(this.typeScript){
+      filter.push(Tag.TYPESCRIPT)
+    }
+
+    if(this.python || this.angular || this.java || this.cSharp || this.typeScript){
+      this.isFiltering = true;
+    }
+    else{
+      this.isFiltering = false
+    }
+
+    this.projects = this.projectService.GetProjectByTag(filter);
+  }
+
+  Reset(){
+    this.python = false;
+    this.angular = false;
+    this.java = false;
+    this.cSharp = false;
+    this.typeScript = false;
+    this.isFiltering = false;
+
     this.projects = this.projectService.GetAllProjects();
   }
 }
